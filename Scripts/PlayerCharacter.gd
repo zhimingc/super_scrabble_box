47ef96f31cbbb1f52d9add41c25d6c924e3977f1
Zhiming Chen <zhi.m.ch@gmail.com>
2020-06-11 09:01:51 +1000

    Initial Godot project commit

---
+++ Scripts/PlayerCharacter.gd
@@ -0,0 +1,46 @@
+extends KinematicBody2D
+
+export var runSpeed = 2.5
+export var jumpForce = 100.0
+export var mass = 10.0
+export var drag = Vector2(0.875,0.925)
+
+var acceleration = Vector2()
+var velocity = Vector2()
+var grav_suppress = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	velocity = Vector2.ZERO
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	acceleration = Vector2()
+	if Input.is_action_pressed("ui_left"):
+		acceleration.x -= runSpeed
+	if Input.is_action_pressed("ui_right"):
+		acceleration.x += runSpeed
+	if is_on_floor():
+		velocity.y = 0.0		
+		if Input.is_action_pressed("ui_up"):
+			jump()	
+	else:
+		acceleration.y += GlobalConstants.gravity * mass
+	
+	update_jump_forces(delta)
+	update_internal_forces(delta)
+	
+	velocity += acceleration * 100;
+	var collision_info = move_and_slide(velocity * delta, Vector2.UP)
+	
+
+func update_jump_forces(delta):
+	if is_on_ceiling():
+		velocity.y = 0.0
+
+func update_internal_forces(delta):
+	velocity *= drag
+	
+func jump():
+	acceleration.y -= jumpForce
+
